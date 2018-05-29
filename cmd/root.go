abb00a70ffa44d3497fae5afed1500955c558109
Clay Dunston <dunstontc@gmail.com>
2018-05-28 23:18:06 -0500

    add files for editors

--- cmd/root.go
+++ cmd/root.go
@@ -35,19 +35,20 @@ var cfgFile string
 var rootCmd = &cobra.Command{
 	Use:   "cobra",
 	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
+	Long: `
+A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
+	// Uncomment the following line if your bare application has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
+// This is called by main.main().
+// It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
@@ -59,12 +60,10 @@ func init() {
 	cobra.OnInitialize(initConfig)
 
 	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
+	// Cobra supports persistent flags, which, if defined here, will be global for your application.
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra.yaml)")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
+	// Cobra also supports local flags, which will only run when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
